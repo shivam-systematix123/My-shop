ab96eadf68d8c15163862d13452cf487d10e575c
shivam-systematix123 <79147218+shivam-systematix123@users.noreply.github.com>
2021-02-21 19:50:48 +0530

    Product Model And Memory

---
+++ myshop/Myshop.core/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myshop.core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        [Range(0,10000)]
+        public Decimal Price { get; set; }
+        public string catagory { get; set; }
+        public string image { get; set; }
+
+        public Product()
+        {
+
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ myshop/myshop.dataaccess.sql/productrepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using Myshop.core;
+
+namespace myshop.dataaccess.sql
+{
+    class productrepo
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+    }
+}
